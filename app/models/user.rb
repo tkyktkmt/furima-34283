ad31cac9415e02ffb667c29d1f6c642f0a223ece
tkyktkmt <tkyktkmt@gmail.com>
2021-04-15 22:02:24 +0900

    テーブル作成、バリデーション設定、viewファイル設置

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :family_name_kanji, presence: true
+  validates :first_name_kanji, presence: true
+  validates :family_name_katakana, presence: true
+  validates :first_name_katakana, presence: true
+  validates :birthday, presence: true
+end
