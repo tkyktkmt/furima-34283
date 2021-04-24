936e7f2772252e8f01d5990b49d37bd0f07316df
tkyktkmt <tkyktkmt@gmail.com>
2021-04-24 09:30:42 +0900

    テストコード実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -15,6 +15,101 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Product name can't be blank" )
     end
-    
+
+    it "detailが空では出品できないこと" do
+      @item.detail = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Detail can't be blank")
+    end
+
+    it "priceが空では出品できないこと" do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it "category_idが空では出品できないこと" do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category is not a number")
+    end
+
+    it "status_idが空では出品できないこと" do
+      @item.status_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status is not a number")
+    end
+
+    it "shipping_fee_burden_idが空では出品できないこと" do
+      @item.shipping_fee_burden_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee burden is not a number")
+    end
+
+    it "shipping_date_idが空では出品できないこと" do
+      @item.shipping_date_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping date is not a number")
+    end
+
+    it "province_idが空では出品できないこと" do
+      @item.province_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Province can't be blank")
+    end
+
+    it "ユーザーが紐づいていなければ出品できないこと" do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
+
+    it "imageが添付されていなければ出品できないこと" do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it "priceが半角でなければ出品できないこと" do
+      @item.price = "３３３"
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+
+    it "priceが300未満では出品できないこと" do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+
+    it "priceが9999999より大きいと出品できないこと" do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
+
+    it "category_idが1では出品できないこと" do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end 
+
+    it "status_idが1では出品できないこと" do
+      @item.status_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status must be other than 1")
+    end
+
+    it "shipping_fee_burden_idが1では出品できないこと" do
+      @item.shipping_fee_burden_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee burden must be other than 1")
+    end
+
+    it "shipping_date_idが1では出品できないこと" do
+      @item.shipping_date_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping date must be other than 1")
+    end
   end
 end
