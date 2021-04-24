a1a78070a743dcb1dca5fc82386999a6e8b83301
tkyktkmt <tkyktkmt@gmail.com>
2021-04-24 11:52:26 +0900

    販売利益をjavascriptで表示

---
+++ app/javascript/item.js
@@ -0,0 +1,12 @@
+function itemPrice() {
+  const priceInput = document.getElementById("item-price");
+  const addTax = document.getElementById("add-tax-price");
+  const addProfit = document.getElementById("profit");
+  
+  priceInput.addEventListener("input", function(){
+    const inputValue = priceInput.value;
+    addTax.innerHTML = Math.floor(inputValue * 0.1);
+    addProfit.innerHTML = Math.floor(inputValue * 0.9);
+  })
+}
+window.addEventListener('load', itemPrice)
