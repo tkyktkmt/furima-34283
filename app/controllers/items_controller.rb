5cba6f166716880c15a8bfd8983337c331dbb95e
tkyktkmt <tkyktkmt@gmail.com>
2021-04-25 09:58:34 +0900

    表示順変更（昇順→降順）、ダミー画像表示切り替え機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
     @items = Item.includes(:user).order(created_at: "DESC" )
-    # @items = Item.new
+    # @items = nil
   end
 
   def new
