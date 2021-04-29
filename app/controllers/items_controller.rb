b0a9c32765477110e10bd837677adf6b44d828b0
tkyktkmt <tkyktkmt@gmail.com>
2021-04-29 17:08:49 +0900

    rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_items, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
-  
+
   def index
     @items = Item.includes(:user).order(created_at: 'DESC')
   end
