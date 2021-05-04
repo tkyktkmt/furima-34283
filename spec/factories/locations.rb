cb01e43eaec14592244abd93510e6c4a012451b8
tkyktkmt <tkyktkmt@gmail.com>
2021-05-04 19:13:14 +0900

    テストコード実装中

--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :location do
-    
+
   end
 end
---
+++ spec/factories/order_location.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :order_location do
+    address = Gimei.address
+
+    postal_code { Faker::Number.number(digits: 3) + '-' + Faker::Number.number(digits: 4) }
+    province_id { Faker::Number.within(range: 1..47) }
+    city { address.city.kanji }
+    address_line1 { Faker::Address.building_number }
+    phone_number {Faker::Number.between(from: 1, to: 99999999999)}
+  end
+end
+
+# city { address.city.kanji address.town.kanji }
